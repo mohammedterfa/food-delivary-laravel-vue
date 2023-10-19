c8b791b294c90eef9fa33153f6f0274f5b8d86e1
mohammedterfa <mohammedterfa@gmail.com>
2023-10-19 21:21:09 +0300

    Users, Roles and Permissions

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RoleName;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->createAdminUser();
+    }
+
+    public function createAdminUser()
+    {
+        User::create([
+            'name'     => 'Admin User',
+            'email'    => 'admin@admin.com',
+            'password' => bcrypt('12345678'),
+        ])->roles()->sync(Role::where('name', RoleName::ADMIN->value)->first());
+    }
+}
