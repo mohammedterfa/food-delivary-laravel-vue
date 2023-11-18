548fddf07d9e55df94a624c5247bfb4855797aa9
mohammedterfa <mohammedterfa@gmail.com>
2023-11-18 19:50:02 +0300

    fix issue

--- app/Http/Controllers/Vendor/MenuController.php
+++ app/Http/Controllers/Vendor/MenuController.php
@@ -4,21 +4,26 @@ namespace App\Http\Controllers\Vendor;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Services\CategoryService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class MenuController extends Controller
 {
+    public function __construct(
+        public CategoryService $categoryService
+    ) {
+    }
+
     public function index(): Response
     {
         $this->authorize('category.viewAny');
 
         return Inertia::render('Vendor/Menu', [
-            'categories' => Category::query()
-                ->where('restaurant_id', auth()->user()->restaurant_id)
-                ->with('products')
-                ->get(),
+            'categories' => $this->categoryService->getRestaurantCategories(
+                withProducts: true
+            ),
         ]);
     }
 }
