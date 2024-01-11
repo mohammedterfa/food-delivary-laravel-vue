5547818fc1c1e8c25152b5562760acf69b786c29
mohammedterfa <mohammedterfa@gmail.com>
2024-01-11 14:58:23 +0300

    redirect

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function add(Product $product)
+    public function add(Product $product): RedirectResponse
     {
 
     }
