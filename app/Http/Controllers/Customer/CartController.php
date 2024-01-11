dc695609e4cac98983d7b07e1f27f0fa136dac74
mohammedterfa <mohammedterfa@gmail.com>
2024-01-11 14:58:01 +0300

    make cartController

---
+++ app/Http/Controllers/Customer/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function add(Product $product)
+    {
+
+    }
+}
